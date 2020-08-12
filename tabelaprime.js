43277e4342634996b725a0ec617ca83df8d283b1
JC-cmd <61801703+JC-cmd@users.noreply.github.com>
2020-08-12 16:02:28 -0300

    Update tabelaprime.js
    Aqui criei uma função pra gerar uma tabela de números primos com gerar_tabela(n,final), onde n é o número de colunas na tabela e final é o maior número a ser testado

--- tabelaprime.js
+++ tabelaprime.js
@@ -4,8 +4,27 @@
 
 */
 
-
-function gerar_tabela(n,final){
-gerandoprimos(1,final);
-document.write(primes[2]);
+function gerar_tabela(n,final){ // INICIO
+var u = '<table>'; 
+  
+gerandoprimos(1,final); // gerando o vetor primes com os numeros primos
+  
+for(var i=0; i<primes.length; i++){ //percorrendo o vetor gerado
+  
+if(!i){ //essa condicao verifica se está no inicio do for
+u+='<tr>';
+for(var j=1; j<=n; j++) u+='<th>'+j+'</th>';
+u+='</tr>';  
 }
+if(!(i%n)) {
+u+='<tr><td>'+(parseInt(i/n)*n+1) /*parseInt(i/n) devolve o numero da linha na tabela*/+' | '+((parseInt(i/n)+1)*n)+'</td>';
+} else if(i%n==(n-1)) u+='</tr>';
+
+u+='<td>'+primes[i]+'</td>';
+  
+}
+  
+u+='</table>';
+  
+return u;
+} // FINAL
