5b98b66a5eb575d5d2ffc24e5327f71e78b03f25
JC-cmd <61801703+JC-cmd@users.noreply.github.com>
2020-08-11 23:38:38 -0300

    Update primos.js
    Cria uma função para verificar se um numero é primo

--- primos.js
+++ primos.js
@@ -1,1 +1,15 @@
+/*
+Gerando numeros primos recursivamente
+*/
 
+
+
+
+
+function isprime(n,m){ /* verifica se um numero eh primo recursivamente, retorna 1 se for primo e 0 se não for*/
+ 
+if(m==1 && n!=1) return 1; // verifica se o processo recursivo chegou ao final com m=1 e retorna que n é primo
+else if((!(n%m) && m!=n) || n==1) return 0; // verifica se n é divisivel por algum
+return isprime(n,m-1); // insere a recursividade
+   
+}
